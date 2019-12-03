2defad6a7463d5ee46ae76c86eeb6c851f913419
Andrew Williams <1williamsaj@gmail.com>
2019-12-03 03:01:43 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,6 +1,6 @@
 # Create your costume_stores migration here
 
-class CreateCostumeStores < ActiveRecord::Migrate[4.2]
+class CreateCostumeStores < ActiveRecord::Migrat[4.2]
   def change
     create_table :costume_stores do |x|
       x.string :name
