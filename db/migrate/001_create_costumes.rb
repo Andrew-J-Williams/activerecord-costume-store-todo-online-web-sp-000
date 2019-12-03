0887cde057fdac352a1e2f3d607cb1dfc1dd0fdd
Andrew Williams <1williamsaj@gmail.com>
2019-12-03 02:35:39 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,7 +14,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       x.string :name
       x.string :price
       x.integer :size
-      x.string :image
+      x.string :image_url
     end
   end
 end
